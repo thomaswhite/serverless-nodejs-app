61fbe0b85511b949cf80d3b4353474338e2e4535
Thomas <thomas.0007@gmail.com>
2019-03-04 14:05:35 +0000

    copy the code from the article

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express')
+const sls = require('serverless-http')
+const app = express()
+app.get('/', async (req, res, next) => {
+  res.status(200).send('Hello World!')
+})
+
+module.exports.server = sls(app)
